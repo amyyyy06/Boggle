a99388d523b2917d1cc08a9cb7cfee87083f98f1
amyyyy06 <atoki@horizon.csueastbay.edu>
2021-07-14 10:17:23 +0900

    I put pre/post conditions.

--- Dictionary.h
+++ Dictionary.h
@@ -32,33 +32,72 @@ public:
                                                 // that this is not a conversion constructor.
 
     Dictionary& operator=(const Dictionary& otherDict);
+    //Calls the recursive function copyHelper to copy a binary tree in class.
     void LoadDictionaryFile(string filename);
+    // Post: Add all the words in the file to dictionary.
     void SaveDictionaryFile(string filename);
+    // Calls the recursive function to save words to a file. It is a "Wrapper" for the SaveDictionaryHelper function.
 
     void AddWord(string word);
+    // Pre: Tree has been initialized.
+    // Post: Add a word to the tree.
     void MakeEmpty();
+    // Call recursive function destroyHelper to destroy all the nodes in the tree. It is a wrapper function for the destroyHelper.
 
     bool IsWord(string word);
+    // Pre:  Tree has been initialized.
+    // Post: Check a provided word exists in the dictionary.
     bool IsPrefix(string word);
+    // Pre:  Tree has been initialized.
+    // Post: Check at least one word in the dictionary starts with a particular prefix.
     int WordCount();
+    // Number of word in the tree.
+
 
 private:
-
     class Node {
     public:
         // Your node structure here.
         // You can also use a struct if you like.
 
+            //Node* letters = letters*ptr[NUM_CHARS];
+
+               Node* letters[NUM_CHARS];
+               bool isWord;
+
+         // Node();
+          /*{
+            for(int i = 0; i < NUM_CHARS; i++){
+                letters[i]= nullptr;
+            }
+            isWord = false;
+        }
+*/
+
+
     };
 
-    Node* root;
-    int numWords;
+/*
+    struct Node {
+        //Node* letters = NUM_CHARS[26];
+        Node* letters[NUM_CHARS];
+        bool isWord; //= false;
+    };
+*/
+    Node* root{};
+    int numWords{};
 
     // Any private methods you need/want
     // You may change these helpers if you want, but you don't need to.
     void destroyHelper(Node* thisTree);
+    //post: Tree is empty. Destroy all the nodes in the tree.
+
     void copyHelper(Node*& thisTree, Node*& otherTree);
+    //post: Copy all of the values.
+
     void SaveDictionaryHelper(Node* curr, string currPrefix, ofstream& outFile);
+    //post: Save words to a file.
+
 };
 
 #endif //BOGGLE_DICTIONARY_H
