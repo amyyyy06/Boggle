0fa3163a5b93175c0b3f808fc408e1183561a5dc
amyyyy06 <atoki@horizon.csueastbay.edu>
2021-07-15 12:24:20 +0900

    Updated Boggle fill.

--- Boggle.cpp
+++ Boggle.cpp
@@ -15,5 +15,36 @@ Dictionary Boggle::WordsFound() {
     return wordsFound;
 }
 
+Boggle::Boggle() {
+    Dictionary dict("dictionary.txt");
+    board[BOARD_SIZE][BOARD_SIZE]=" ";
+    visited[BOARD_SIZE][BOARD_SIZE]= false;
+
+
+}
+
+Boggle::Boggle(string filename) {
+
+}
+
+void Boggle::SetBoard(string (*board)[4]) {
+
+}
+
+void Boggle::SolveBoard(bool printBoard, ostream &output) {
+
+}
+
+void Boggle::SaveSolve(string filename) {
+
+}
+
+void Boggle::PrintBoard(ostream &output) {
+
+}
+
+int Boggle::SolveBoardHelper(int row, int col, ostream &output) {
+    return 0;
+}
 
 
